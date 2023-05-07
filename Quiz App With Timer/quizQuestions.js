f7dee46c346ae4aefb436c32904b25a54c90f6ad
wowprogrammers <waleedbukhari872@gmail.com>
2023-05-07 09:59:03 +0530

    Quiz Application Initial Set up

---
+++ Quiz App With Timer/quizQuestions.js
@@ -0,0 +1,66 @@
+// Creating an array of questions in which we specify
+// Question Number
+// Question
+// Answer
+// Options
+
+let questions = [
+    {
+    numb: 1,
+    question: "Which type of variable needs initialization at the time of declaration",
+    answer: "Variable Declared with const keyword",
+    options: [
+      "Variable Declared with let keyword keyword",
+      "Variable Declared with const keyword",
+      "Variable Declared with var keyword",
+      "None of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which one is the Javascript library?",
+    answer: "React js",
+    options: [
+      "Angular",
+      "Vue js",
+      "React js",
+      "All of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which HTML tag we can use to add image in html?",
+    answer: "<img src = ''>",
+    options: [
+      "<img src = ''>",
+      "<image src = ''>",
+      "<img source = ''>",
+      "None"
+    ]
+  },
+  
+//   Similarly You can add as many Question You want...
+
+];
